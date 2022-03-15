ae70d209a6796061ef1cc68769c7b603d76c48c9
shashankshukla858@gmail.com <shashankshukla858@gmail.com>
2022-03-15 17:36:58 +0530

    first day of work

---
+++ commands/help.js
@@ -0,0 +1,18 @@
+function help()
+    {
+       console.log(`these are some cli commands:
+       1.node main.js tree<path>
+       2.node main.js oraganize<path>
+       3.node main.js help`);
+
+     
+    }
+
+
+    module.exports={
+        //key value
+        help:help,
+        //haathi:help
+        //ghoda:abc
+
+    }
---
+++ main.js
@@ -0,0 +1,20 @@
+let helpFunc = require("./commands/help");
+let inputArr=process.argv.slice(2);
+let command=inputArr[0];
+let path=inputArr[1];
+switch(command)
+{
+    case "tree":
+        console.log("ok"+path);
+        break;
+    case "organize":
+        console.log("prganize"+path);
+        break;
+    case "help":
+        helpFunc.help();
+        break;
+    default:
+        console.log("wrong input");
+        break;
+    
+}
